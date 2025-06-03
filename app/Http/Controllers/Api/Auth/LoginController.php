7e0aeb7f074225b622d65972b79dfdaf397abe2e
Gideon Sipoi <giddysipoi@gmail.com>
2025-06-03 17:48:59 +0300

    feat: user login api functionality

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserLoginRequest;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function loginUser(UserLoginRequest $request)
+    {
+        // return response()->json([
+        //     'message' => 'Login functionality not implemented yet.',
+        // ], 501); // 501 Not Implemented
+
+        $validated = $request->validated();
+
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => $user,
+                'token' => $token,
+                'expiration' => now()->addMinutes((int) config('sanctum.expiration', 10)), // defaults to 10 minutes
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Invalid credentials',
+        ], 401); // 401 Unauthorized
+
+    }
 }
